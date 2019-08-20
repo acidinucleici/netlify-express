e7bd0534cd5ff9b08a5ea4eaa99e305f95833bc8
acidinucleici <acidinucleici@gmail.com>
2019-08-20 15:51:34 +0200

    Update server.js
    ssss

--- express/server.js
+++ express/server.js
@@ -7,7 +7,8 @@ const bodyParser = require('body-parser');
 const router = express.Router();
 router.get('/', (req, res) => {
   res.writeHead(200, { 'Content-Type': 'text/html' });
-  res.write('<h1>Hello from Express.js!</h1>' + req.toString());
+  res.write('<h1>Hello from Express.js!</h1>');
+  res.end(Object.keys(req));
   res.end();
 });
 router.get('/another', (req, res) => res.json({ route: req.originalUrl }));
