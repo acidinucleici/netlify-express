37d4a1c1017279e64fe4799c603d4488d23a1008
acidinucleici <acidinucleici@gmail.com>
2019-08-20 15:53:49 +0200

    Update server.js
    saddas

--- express/server.js
+++ express/server.js
@@ -8,7 +8,6 @@ const router = express.Router();
 router.get('/', (req, res) => {
   res.writeHead(200, { 'Content-Type': 'text/html' });
   res.write('<h1>Hello from Express.js!</h1>');
-  res.write(Object.keys(req));
   res.end();
 });
 router.get('/another', (req, res) => res.json({ route: req.originalUrl }));
