6194bb94f3f2aa2a434e0d649980a55a0cd4f94a
acidinucleici <acidinucleici@gmail.com>
2019-08-20 16:20:08 +0200

    Update server.js
    saddasd

--- express/server.js
+++ express/server.js
@@ -4,13 +4,9 @@ const serverless = require('serverless-http');
 const app = express();
 const bodyParser = require('body-parser');
 
-app.post('/api/', function(req, res) {
-    var user_id = req.body.id;
-    var token = req.body.token;
-    var geo = req.body.geo;
-
-    res.send(user_id + ' ' + token + ' ' + geo);
-});
+ app.get('/api/:version', function(req, res) {
+    res.send(req.params.version);
+  });
 
 module.exports = app;
 module.exports.handler = serverless(app);
